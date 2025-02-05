cbdb4365001b9449cabd40c13defb595544f7f20
Song Khoi <songkhoi123@gmail.com>
2025-02-06 00:48:18 +0700

    fix clang header

--- src/kilo.c
+++ src/kilo.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <io.h>
+#define STDIN_FILENO 0
+#else
+#include <unistd.h>
+#endif
+
 int main()
 {
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+        ;
     return 0;
 }
