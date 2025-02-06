f84210089f5a737c6b1a6995198e9c6599414e11
Song Khoi <songkhoi123@gmail.com>
2025-02-06 08:08:28 +0700

    add raw mode

--- src/kilo.c
+++ src/kilo.c
@@ -3,12 +3,31 @@
 #ifdef _WIN32
 #include <io.h>
 #define STDIN_FILENO 0
+#include <windows.h>
 #else
+#include <termios.h>
 #include <unistd.h>
 #endif
 
+void enableRawMode()
+{
+#ifdef _WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD  mode;
+    GetConsoleMode(hStdin, &mode);
+    mode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
+    SetConsoleMode(hStdin, mode);
+#else
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+#endif
+}
+
 int main()
 {
+    enableRawMode();
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
         ;
